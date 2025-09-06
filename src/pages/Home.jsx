d9b3ad39f6313fe66c7188a62c22fc89749e254b
burakkgolcuk <burakkgolcuk@gmail.com>
2025-09-06 10:35:40 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
+// src/components/Home.jsx
 import React from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
+
+// ICONLAR
+import icon1 from "../assets/iteration-2-images/icons/1.svg";
+import icon2 from "../assets/iteration-2-images/icons/2.svg";
+import icon3 from "../assets/iteration-2-images/icons/3.svg";
+import icon4 from "../assets/iteration-2-images/icons/4.svg";
+import icon5 from "../assets/iteration-2-images/icons/5.svg";
+import icon6 from "../assets/iteration-2-images/icons/6.svg";
+
+// KART GÖRSELLERİ (CTA)
+import kart1 from "../assets/iteration-2-images/cta/kart-1.png";
+import kart2 from "../assets/iteration-2-images/cta/kart-2.png";
+import kart3 from "../assets/iteration-2-images/cta/kart-3.png";
+
+// ÜRÜN GÖRSELLERİ
+import food1 from "../assets/iteration-2-images/pictures/food-1.png";
+import food2 from "../assets/iteration-2-images/pictures/food-2.png";
+import food3 from "../assets/iteration-2-images/pictures/food-3.png";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -22,32 +41,33 @@ export default function Home() {
         </div>
       </header>
 
+      {/* Kategoriler */}
       <nav className="categories">
         <span className="active">
-          <img src="src\assets\iteration-2-images\icons\1.svg" alt="Ramen" /> Yeni! Kore
+          <img src={icon1} alt="Ramen" /> Yeni! Kore
         </span>
         <span>
-          <img src="src\assets\iteration-2-images\icons\2.svg" alt="Pizza" /> Pizza
+          <img src={icon2} alt="Pizza" /> Pizza
         </span>
         <span>
-          <img src="src\assets\iteration-2-images\icons\3.svg" alt="Burger" /> Burger
+          <img src={icon3} alt="Burger" /> Burger
         </span>
         <span>
-          <img src="src\assets\iteration-2-images\icons\4.svg" alt="Fries" /> Kızartmalar
+          <img src={icon4} alt="Fries" /> Kızartmalar
         </span>
         <span>
-          <img src="src\assets\iteration-2-images\icons\5.svg" alt="Fast Food" /> Fast food
+          <img src={icon5} alt="Fast Food" /> Fast food
         </span>
         <span>
-          <img src="src\assets\iteration-2-images\icons\6.svg" alt="Drinks" /> Gazlı İçecek
+          <img src={icon6} alt="Drinks" /> Gazlı İçecek
         </span>
       </nav>
 
+      {/* Öne çıkan teklifler (arka planı import ile veriyoruz) */}
       <section className="offer-section">
         <div
           className="offer-card"
-          style={{ backgroundImage: "url('/images/iteration-2-images/cta/kart-1.png')" }}
-
+          style={{ backgroundImage: `url(${kart1})` }}
         >
           <div className="offer-text">
             <h2>Özel Lezzetus</h2>
@@ -59,8 +79,7 @@ export default function Home() {
         <div className="offer-right">
           <div
             className="offer-card black"
-            style={{ backgroundImage: "url('/images/iteration-2-images/cta/kart-2.png')" }}
-
+            style={{ backgroundImage: `url(${kart2})` }}
           >
             <div className="offer-text">
               <h3>Hackathon Burger Menü</h3>
@@ -70,9 +89,7 @@ export default function Home() {
 
           <div
             className="offer-card yellow"
-            style={{ backgroundImage: "url('/images/iteration-2-images/cta/kart-3.png')" }}
-
-
+            style={{ backgroundImage: `url(${kart3})` }}
           >
             <div className="offer-text">
               <p>
@@ -85,34 +102,35 @@ export default function Home() {
         </div>
       </section>
 
+      {/* En çok satanlar */}
       <section className="top-products">
         <div className="section-subtitle">en çok paketlenen menüler</div>
         <h2 className="section-title">Acıktıran Kodlara Doyuran Lezzetler</h2>
 
         <div className="categories">
           <span className="active">
-            <img src="src\assets\iteration-2-images\icons\1.svg" alt="Ramen Icon" /> Ramen
+            <img src={icon1} alt="Ramen Icon" /> Ramen
           </span>
           <span style={{ backgroundColor: "black", color: "white" }}>
-            <img src="/src\assets\iteration-2-images\icons\2.svg" alt="Pizza Icon" /> Pizza
+            <img src={icon2} alt="Pizza Icon" /> Pizza
           </span>
           <span>
-            <img src="src\assets\iteration-2-images\icons\3.svg" alt="Burger Icon" /> Burger
+            <img src={icon3} alt="Burger Icon" /> Burger
           </span>
           <span>
-            <img src="/src\assets\iteration-2-images\icons\4.svg" alt="Fries Icon" /> French fries
+            <img src={icon4} alt="Fries Icon" /> French fries
           </span>
           <span>
-            <img src="src\assets\iteration-2-images\icons\5.svg" alt="Fast Food Icon" /> Fast food
+            <img src={icon5} alt="Fast Food Icon" /> Fast food
           </span>
           <span>
-            <img src="src\assets\iteration-2-images\icons\6.svg" alt="Soft Drinks Icon" /> Soft drinks
+            <img src={icon6} alt="Soft Drinks Icon" /> Soft drinks
           </span>
         </div>
 
         <div className="product-list">
           <div className="product-card">
-            <img src="src\assets\iteration-2-images\pictures\food-1.png" alt="Terminal Pizza" />
+            <img src={food1} alt="Terminal Pizza" />
             <h4>Terminal Pizza</h4>
             <div className="product-info">
               <span>4.9</span>
@@ -122,7 +140,7 @@ export default function Home() {
           </div>
 
           <div className="product-card">
-            <img src="src\assets\iteration-2-images\pictures\food-2.png" alt="Acı Pizza" />
+            <img src={food2} alt="Acı Pizza" />
             <h4>Position Absolute Acı Pizza</h4>
             <div className="product-info">
               <span>4.9</span>
@@ -132,7 +150,7 @@ export default function Home() {
           </div>
 
           <div className="product-card">
-            <img src="src\assets\iteration-2-images\pictures\food-3.png" alt="Tavuklu Burger" />
+            <img src={food3} alt="Tavuklu Burger" />
             <h4>useEffect Tavuklu Burger</h4>
             <div className="product-info">
               <span>4.9</span>
